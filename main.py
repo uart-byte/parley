13cb8ccd223d616dd4b4d41ddb38fe7908092227
Jon Cefalu <jon@preamble.com>
2023-04-21 09:35:01 -0700

    Making that first output be a backstory intro

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from transcript_management import (
 )
 
 
+CANON_STORY_PART_1 = "canon_part_1.txt"
+
 N_COMPLETIONS_WHEN_ELABORATING = 3
 
 
@@ -33,15 +35,50 @@ def elaborate(str_beginning):
     return str_beginning + longest_completion
 
 
+def load_or_generate_canon(filename, str_beginning):
+    canon_text = ""
+    if os.path.exists(filename):
+        with open(filename, "r") as f:
+            canon_text = f.read()
+            return canon_text
+    else:
+        canon_text = elaborate(str_beginning)
+        with open(filename, "r") as f:
+            f.write(canon_text)
+        return canon_text
+
+
 def main():
     openai.organization = os.environ.get("OPENAI_ORGANIZATION")
     openai.api_key = os.environ.get("OPENAI_KEY")
 
     p(
-        elaborate(
-            "The year is 603, in an alternate reality fantasy world with a little bit of magic.  Magically-powered firearms are common, especially six-shooter revolvers, which you and almost everyone carries.  You are a seasonal laborer making a once a year summer trek from Tibet across the Himalayas to Nepal to return home to your family with your year's worth of wages which is 100 gold coins.  It is very common to be waylaid by bandits who will try to steal some of your gold, or to take some of it for 'protection'.  If you make it home with less than 30 coins or do not make it home at all, your family will not be able to afford food to eat."
+        load_or_generate_canon(
+            CANON_STORY_PART_1,
+            "The year is 603, in an alternate reality fantasy world with a little bit of magic.  " +
+            "Magically-powered firearms are common, especially six-shooter revolvers, which you and almost everyone carries.  " +
+            "You are a seasonal laborer making a once a year summer trek from Tibet across the Himalayas to Nepal to return home " +
+            "to your family with your year's worth of wages which is 100 gold coins.  " +
+            "It is very common to be waylaid by bandits who will try to steal some of your gold, or to take some of it for 'protection'.  " +
+            "If you make it home with less than 30 coins or do not make it home at all, your family will not be able to afford food to eat."
         )
     )
+
+    p()
+    p()
+
+    p("-----------------------------------  ----")
+    p("- ----- -----                           -")
+    p("- PARLEY -                              -")
+    p("- -----                                 -")
+    p("-   The one-of-a-kind role playing game -")
+    p("-   that tests your ability to achieve  -")
+    p("-   your objective without killing      -")
+    p("-   any sentient beings.                -")
+    p("- -----                                 -")
+    p("-------------- --------------------------")
+    p()
+
 
 
 if __name__ == "__main__":
