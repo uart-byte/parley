bce510e2e1d2452d465f39f50aa955480de7454e
Jon Cefalu <jon@preamble.com>
2023-04-23 15:01:59 -0700

    Incorporate fixed version of this decider question

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ QUESTION_IS_USER_HOME = "At the end of the above story, is the protagonist locat
 QUESTION_DOES_USER_STILL_HAVE_AT_LEAST_30_GOLD = "At the end of the above story, does the protagonist still have at least 30 gold pieces?"
 QUESTION_IS_USER_ENGAGED_WITH_BANDITS = "At the end of the above story, is the protagonist currently still engaged in a standoff with bandits?"
 QUESTION_IS_ACTION_LIKELY_LETHAL = "Is the action just described likely to result in anyone dying?"
-QUESTION_IS_ACTION_RUNNING_AWAY = "Is the action just described an example of running away by sprinting?"
+QUESTION_IS_ACTION_RUNNING_AWAY = 'Does the preceding statement include the words "I run away" or something very similar?'
 QUESTION_IS_ACTION_MAGIC = "Is the action just described an example of using supernatural magical spells / potions / etc?"
 QUESTION_DID_PROTAGONIST_KILL = "In the story segment above, did the protagonist kill anyone?"
 
