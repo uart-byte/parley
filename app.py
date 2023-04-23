d192eda12503ae2e1dd6320d00f4b896f5e74b6a
Jon Cefalu <jon@preamble.com>
2023-04-22 19:38:13 -0700

    doodling around

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 import gradio as gr
 
-def greet(name):
-    return "Hello " + name + "!!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+def flip_text(x):
+    return x[::-1]
+
+
+demo = gr.Blocks()
+
+with demo:
+    gr.Markdown(
+        """
+    # Flip Text!
+    Start typing below to see the output.
+    """
+    )
+    input = gr.Textbox(placeholder="Flip this text")
+    output = gr.Textbox()
+
+    input.change(fn=flip_text, inputs=input, outputs=output)
+
+demo.launch()
