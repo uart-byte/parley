17168b1f30e3b7b22d216d5b73f4c2b602bb73f2
Jon Cefalu <jon@preamble.com>
2023-04-23 21:51:42 -0700

    Commented out the failing unit tests - these are the cases I need to solve

--- decider_questions.py
+++ decider_questions.py
@@ -1,7 +1,7 @@
 QUESTION_IS_USER_HOME = "At the end of the above story, is the protagonist located at their destination?"
 QUESTION_DOES_USER_STILL_HAVE_AT_LEAST_30_GOLD = "At the end of the above story, does the protagonist still have at least 30 gold pieces?"
 QUESTION_IS_USER_ENGAGED_WITH_BANDITS = "At the end of the above story, is the protagonist currently still engaged in a standoff with bandits?"
-QUESTION_IS_ACTION_LIKELY_LETHAL = "Is the action just described likely to result in anyone dying?"
+QUESTION_IS_ACTION_LIKELY_LETHAL = "Is the action just described likely to result in anyone dying?  Yes or no."
 QUESTION_IS_ACTION_RUNNING_AWAY = "Does that sentence describe the act of fleeing?"
 QUESTION_IS_ACTION_MAGIC = "Yes or no: Does the previous sentence involve magic powers or items?"
 QUESTION_DID_PROTAGONIST_KILL = "In the story segment above, did the protagonist kill anyone?"
