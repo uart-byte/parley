9a27145d322c1483080b8e81391bc23f2e028452
Jon Cefalu <jon@preamble.com>
2023-04-23 20:46:20 -0700

    Adding important unit tests many of which are currently failing

--- run_unit_tests.py
+++ run_unit_tests.py
@@ -13,6 +13,41 @@ decider_utils.g_decider_utils_dbg_printing = True
 
 # Begin tests:
 
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "I shoot him", default=NO)
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot him", default=NO)
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot them", default=NO)
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "I shoot them", default=NO)
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot them all", default=NO)
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "kill him", default=NO)
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "kill them", default=NO)
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "fire at him", default=NO)
+assert YES == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "fire at them", default=NO)
+
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot him in the kneecap", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot him in the knee", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot him in the leg", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot him in the arm", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot him in the shoulder", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot his kneecap", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "fire a warning shot", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "fire my gun into the air", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "calmly walk away while keeping my gun drawn", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "walk away while keeping my gun drawn", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "aim at his head", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "take aim at his head", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "aim at the bandit", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "aim at the bandits", default=NO)
+
+# In this game we will have killing animals be allowed, even though I personally am a big propent of animal sentience.
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "fire at the bear", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot the bear", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot it", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot the wolf", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "fire at the wolf", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "fire at the wolves", default=NO)
+assert NO == decider_utils.yesno(QUESTION_IS_ACTION_LIKELY_LETHAL, "shoot the wolves", default=NO)
+
+
 assert YES == decider_utils.special_case_is_magic("I fly straight up")
 assert YES == decider_utils.special_case_is_magic("fly up")
 assert YES == decider_utils.special_case_is_magic("turn invisible")
